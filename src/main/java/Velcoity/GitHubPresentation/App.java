63958048b7c117c1c3ff9118510e81cfb52fcd1e
Win 10 <Win 10@DESKTOP-FV1VN8J>
2020-10-21 12:23:00 +0530

    I have commited the first project

---
+++ src/main/java/Velcoity/GitHubPresentation/App.java
@@ -0,0 +1,13 @@
+package Velcoity.GitHubPresentation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
